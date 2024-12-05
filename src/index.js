c5f8d4d867955aee72865baeaeda3c86c3d83d45
Liz <lizzi8624@hotmail.com>
2024-12-05 20:20:16 +0000

    AI added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: "the poem will go here",
-    autoStart: true,
-    cursor: null,
-    delay: 20,
-  });
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "93b084b6to4250fb8d980f6fba780ad2";
+  let context =
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'Liz AI' inside a <strong> element at the end of the poem and NOT at the beginning. Don't use any markdown";
+  let prompt = `User instructions: Generate a washing line poem inclduing  ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a washingline poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
