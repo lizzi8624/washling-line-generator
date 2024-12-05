ef039a3f3ebad77239f5b9a303be8f48dd62fb72
Liz <lizzi8624@hotmail.com>
2024-12-05 19:50:24 +0000

    Javascript addition 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("hello world")
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "the poem will go here",
+    autoStart: true,
+    cursor: null,
+    delay: 20,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
