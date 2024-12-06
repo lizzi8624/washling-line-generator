a219120688d0c501c096245f3ca5cefa0d302833
Liz <lizzi8624@hotmail.com>
2024-12-06 10:08:00 +0000

    rhyme

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "93b084b6to4250fb8d980f6fba780ad2";
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'Liz AI' inside a <strong> element at the end of the poem and NOT at the beginning. Don't use any markdown";
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'Liz AI' inside a <strong> element at the end of the poem and NOT at the beginning. Don't use any markdown. It can't rhyme";
   let prompt = `User instructions: Generate a washing line poem inclduing  ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
